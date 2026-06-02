51c5564a3d4389e66ce604aea5e432aceebf84ba
BlueNyang <dksrbxo@bluenyang.kr>
2026-06-03 02:45:57 +0900

    feat: add auth schema

---
+++ apps/server/src/database/schema/auth.schema.ts
@@ -0,0 +1,15 @@
+import { pgTable, timestamp, uuid } from 'drizzle-orm/pg-core';
+import { uuidv7 } from 'uuidv7';
+
+import { users } from './user.schema';
+
+export const refreshTokens = pgTable('refresh_tokens', {
+  id: uuid('id')
+    .$defaultFn(() => uuidv7())
+    .primaryKey(),
+  userId: uuid('user_id').references(() => users.id, { onDelete: 'cascade' }),
+  tokenValue: uuid('token_value').notNull().unique(),
+  expiresAt: timestamp('expires_at', { withTimezone: true }).notNull(),
+  createdAt: timestamp('created_at', { withTimezone: true }).defaultNow(),
+  revokedAt: timestamp('revoked_at', { withTimezone: true }),
+});
