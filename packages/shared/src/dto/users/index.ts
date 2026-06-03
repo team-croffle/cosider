f18d244f7d2a2c9c71c7a3a606881d3b9d6f1dd6
BlueNyang <dksrbxo@bluenyang.kr>
2026-06-03 16:41:50 +0900

    remove: remove dto in shared

--- packages/shared/src/dto/users/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './deactivate-request.dto';
-export * from './deactivate-response.dto';
-export * from './restore-request.dto';
-export * from './restore-response.dto';
-export * from './user-profile-response.dto';
-export * from './check-handle-exists-response.dto';
