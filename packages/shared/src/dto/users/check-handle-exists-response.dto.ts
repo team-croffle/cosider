0feac41a83eefec2772b6dbe83c0a4e315667b85
BlueNyang <dksrbxo@bluenyang.kr>
2026-06-03 16:39:47 +0900

    refactor: define with interface and type

--- packages/shared/src/dto/users/check-handle-exists-response.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-
-export class CheckHandleExistsResponseDto {
-  @ApiProperty({
-    example: true,
-    description: '핸들 사용 가능 여부',
-  })
-  is_available: boolean;
-}
