dbc3b9f32865734f9e05e91507ece2f26a4d6c69
VenyVince <redguy0814@gmail.com>
2026-06-11 20:23:06 +0900

    feat: Add jwt authentication infrastructure
    - configure JwtModule
    - Add JwtStrategy
    - Add JwtAuthGuard
    - Add JwtPayload interface
    - Add access and refresh token generators

--- apps/server/src/modules/auth/auth.module.ts
+++ apps/server/src/modules/auth/auth.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
+  imports: [
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.getOrThrow('JWT_SECRET'),
+        signOptions: {
+          expiresIn: '5m',
+        },
+      }),
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
