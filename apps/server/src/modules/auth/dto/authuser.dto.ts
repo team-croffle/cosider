4f78e24ff19d3218703d2127484b75246a1033db
VenyVince <redguy0814@gmail.com>
2026-06-14 19:50:42 +0900

    refactor: refactor Dtos for clearity and consistency

--- apps/server/src/modules/auth/dto/authuser.dto.ts
+++
@@ -1,6 +0,0 @@
-import { IAuthUser } from '../interface/auth-user.interface';
-
-export class AuthUser implements IAuthUser {
-  userId!: string;
-  email!: string;
-}
--- apps/server/src/modules/auth/dto/jwt-payload.dto.ts
+++ apps/server/src/modules/auth/dto/jwt-payload.dto.ts
@@ -1,6 +1,12 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
 import { IJwtPayload } from '../interface/jwt-payload.interface';
 
 export class JwtPayloadDto implements IJwtPayload {
+  @IsNotEmpty()
   userId!: string;
+
+  @IsEmail()
+  @IsNotEmpty()
   email!: string;
 }
