e0d087827f49edbe3e88ab1eb8c9d0b012d0cda0
BlueNyang <dksrbxo@bluenyang.kr>
2026-06-16 23:52:23 +0900

    feat: add media files table interface

--- packages/shared/src/common/common.enum.ts
+++ packages/shared/src/common/common.enum.ts
@@ -3,3 +3,17 @@ export enum EPriority {
   MID = 'MID', // 중간
   HIGH = 'HIGH', // 높음
 }
+
+export enum EFileVisibility {
+  PRIVATE = 'PRIVATE', // 비공개
+  PROJECT = 'PROJECT', // 프로젝트 전체
+  WORKSPACE = 'WORKSPACE', // 워크스페이스 전체
+  PUBLIC = 'PUBLIC', // 전체 공개
+}
+
+export enum EFileRefType {
+  USER = 'USER', // 사용자 프로필 이미지
+  PROJECT = 'PROJECT', // 프로젝트 로고 이미지
+  TASK = 'TASK', // 태스크 첨부파일
+  WORKSPACE = 'WORKSPACE', // 워크스페이스 로고 이미지
+}
--- packages/shared/src/common/index.ts
+++ packages/shared/src/common/index.ts
@@ -1,2 +1,3 @@
 export * from './common.dto';
 export * from './common.enum';
+export * from './common.interface';
