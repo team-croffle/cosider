7103439fb3440a019893c246e9051f87119a4ca7
BlueNyang <dksrbxo@bluenyang.kr>
2026-06-16 23:59:26 +0900

    feat: add media schema and rename interface

--- apps/server/src/database/schema/common.schema.ts
+++ apps/server/src/database/schema/common.schema.ts
@@ -1,7 +1,38 @@
-import { EPriority } from '@cosider/shared';
-import { pgEnum } from 'drizzle-orm/pg-core';
+import { EFileRefType, EFileVisibility, EPriority, IMediaFile } from '@cosider/shared';
+import { bigint, pgEnum, pgTable, text, timestamp, uuid, varchar } from 'drizzle-orm/pg-core';
+import { uuidv7 } from 'uuidv7';
 
 export const priorityEnum = pgEnum(
   'priority_enum',
   Object.values(EPriority) as [string, ...string[]],
 );
+
+// ############### MEDIA FILES ###############
+
+type MediaFileSchema = Record<keyof IMediaFile, unknown>;
+
+export const fileVisibilityEnum = pgEnum(
+  'file_visibility_enum',
+  Object.values(EFileVisibility) as [string, ...string[]],
+);
+
+export const fileRefTypeEnum = pgEnum(
+  'file_ref_type_enum',
+  Object.values(EFileRefType) as [string, ...string[]],
+);
+
+export const mediaFiles = pgTable('media_files', {
+  id: uuid('id')
+    .primaryKey()
+    .$defaultFn(() => uuidv7()),
+  bucketName: varchar('bucket_name', { length: 50 }).notNull(),
+  objectKey: text('object_key').notNull(),
+  originalName: text('original_name').notNull().unique(),
+  mimeType: text('mime_type').notNull(),
+  fileSize: bigint('file_size', { mode: 'number' }).notNull(),
+  visibility: fileVisibilityEnum('visibility').notNull(),
+  refType: fileRefTypeEnum('ref_type').notNull(),
+  refId: uuid('ref_id').notNull(),
+  ownerId: uuid('owner_id').notNull(),
+  createdAt: timestamp('created_at', { withTimezone: true }).defaultNow(),
+} satisfies MediaFileSchema);
--- packages/shared/src/common/common.interface.ts
+++ packages/shared/src/common/common.interface.ts
@@ -1,15 +1,15 @@
 import { EFileRefType, EFileVisibility } from './common.enum';
 
-export interface MediaFile {
+export interface IMediaFile {
   id: string;
   bucketName: string;
   objectKey: string;
   originalName: string;
   mimeType: string;
   fileSize: number;
+  visibility: EFileVisibility;
+  refType: EFileRefType;
   refId: string;
   ownerId: string | null;
-  refType: EFileRefType;
-  visibility: EFileVisibility;
   createdAt: string;
 }
