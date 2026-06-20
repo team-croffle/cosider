eb179a3335502d6ff598041c44fd8762a193d591
BlueNyang <dksrbxo@bluenyang.kr>
2026-06-21 00:33:41 +0900

    feat: add providers on auth module

--- apps/server/src/modules/auth/auth.module.ts
+++ apps/server/src/modules/auth/auth.module.ts
@@ -1,13 +1,19 @@
 import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtAuthGuard } from './guard/jwt-auth.guard';
+import { LocalAuthGuard } from './guard/local-auth.guard';
+import { RefreshGuard } from './guard/refresh.guard';
 import { JwtStrategy } from './strategy/jwt.strategy';
+import { UserCredentialService } from './user-credential.service';
 
 @Module({
   imports: [
+    PassportModule.register({ defaultStrategy: 'jwt', session: false }),
     JwtModule.registerAsync({
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
@@ -20,6 +26,17 @@ import { JwtStrategy } from './strategy/jwt.strategy';
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService, JwtStrategy],
+  providers: [
+    // Services
+    AuthService,
+    UserCredentialService,
+    // Passports
+    JwtStrategy,
+
+    //Guards
+    LocalAuthGuard,
+    JwtAuthGuard,
+    RefreshGuard,
+  ],
 })
 export class AuthModule {}
