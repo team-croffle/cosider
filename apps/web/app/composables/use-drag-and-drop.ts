c1b053a81e95d5735ae7d4d6b8aaaf105f69670f
BlueNyang <dksrbxo@bluenyang.kr>
2026-07-01 01:18:59 +0900

    feat: drag and drop composable

---
+++ apps/web/app/composables/use-drag-and-drop.ts
@@ -0,0 +1,42 @@
+export function useDragAndDrop<T>(initList: T[]) {
+  const list = ref<T[]>(initList);
+  const draggedIndex = ref<number | null>(null);
+  const dropTargetIndex = ref<number | null>(null);
+
+  function onDragStart(idx: number) {
+    draggedIndex.value = idx;
+  }
+
+  function onDragEnter(idx: number) {
+    if (draggedIndex.value === idx) {
+      return;
+    }
+    dropTargetIndex.value = idx;
+  }
+
+  function onDrop() {
+    if (draggedIndex.value === null || dropTargetIndex.value === null) {
+      return;
+    }
+
+    const [item] = list.value.splice(draggedIndex.value, 1);
+    if (item !== undefined) {
+      list.value.splice(dropTargetIndex.value, 0, item);
+    }
+
+    // TODO: Server에 변경된 리스트 저장 API를 Call.
+    // 해당 logic이 내장된 것이 아니라, 해당 API를 Call하는 CallBack함수를 인자로 받도록 변경
+
+    draggedIndex.value = null;
+    dropTargetIndex.value = null;
+  }
+
+  return {
+    list,
+    draggedIndex,
+    dropTargetIndex,
+    onDragStart,
+    onDragEnter,
+    onDrop,
+  };
+}
